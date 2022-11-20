5c9fbba33a769a7d11132beff7eac1ce7d6e54d4
Patrick Nelsen <pnelsen129@gmail.com>
2022-11-20 13:44:27 -0700

    Added subnet ids as output from network module

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -2,3 +2,8 @@ output "vpc_id" {
     description = "The id of the vpc"
     value       = module.vpc.vpc_id
 }
+
+output "subnet_ids" {
+    description = "The ids of created subnets"
+    value       = module.vpc.public_subnets
+}
