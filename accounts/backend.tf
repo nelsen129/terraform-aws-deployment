dd95ad3d77e72a645b058206b14cb76807e75f6e
Patrick Nelsen <pnelsen129@gmail.com>
2022-12-05 21:19:42 -0700

    feat: add dynamodb table to backend.tf

--- accounts/backend.tf
+++ accounts/backend.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-aws-deployment-state-bucket"
+    dynamodb_table = "terraform-aws-deployment-dynamodb-lock-state"
     key    = "accounts/terraform.tfstate"
     region = "us-east-1"
   }
