c52fbb287fbaa661740c7ad4ef004c867852334e
Patrick Nelsen <55818910+nelsen129@users.noreply.github.com>
2022-12-06 18:56:33 -0700

    29 create accounts (#30)
    * feat: create accounts

--- accounts/main.tf
+++ accounts/main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = var.region
-}
-
 module "backend" {
   source = "../modules/backend"
 
@@ -13,4 +9,12 @@ module "backend" {
 
 module "accounts" {
   source = "../modules/accounts"
+
+  account_emails = var.account_emails
+  account_names  = var.account_names
+
+  role_name         = var.account_role_name
+  close_on_deletion = var.account_close_on_deletion
+
+  tags = var.tags
 }
--- accounts/terraform.tfvars
+++ accounts/terraform.tfvars
@@ -1,8 +1,12 @@
 name = "terraform-aws-deployment"
 
+region = "us-east-1"
+
+account_role_name = "OrganizationAccountAccessRole"
+
+account_close_on_deletion = true
+
 tags = {
   project     = "terraform-aws-deployment"
   environment = "accounts"
 }
-
-region = "us-east-1"
