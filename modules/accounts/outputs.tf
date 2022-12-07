04b19ae2530597ead5f691774396e8c1c3ad190a
Patrick Nelsen <pnelsen129@gmail.com>
2022-12-06 20:43:35 -0700

    feat: add account_ids output to accounts module

--- modules/accounts/outputs.tf
+++ modules/accounts/outputs.tf
@@ -12,3 +12,13 @@ output "organization_id" {
   description = "ID of the organization"
   value       = resource.aws_organizations_organization.this.id
 }
+
+output "organization_management_account_id" {
+  description = "ID of the management account"
+  value       = resource.aws_organizations_organization.this.master_account_id
+}
+
+output "account_ids" {
+  description = "IDs of the managed accounts"
+  value       = resource.aws_organizations_account.this.*.id
+}
