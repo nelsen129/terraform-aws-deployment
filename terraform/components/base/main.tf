a0f68d4b35c80f3f4ea541ff7363977403c927fb
Patrick Nelsen <pnelsen129@gmail.com>
2023-01-29 20:34:18 -0700

    feat: add base component

--- terraform/components/base/main.tf
+++ terraform/components/base/main.tf
@@ -1,5 +1,5 @@
 module "backend" {
-  source = "../modules/backend"
+  source = "../../modules/backend"
 
   bucket_name   = "${var.name}-state-bucket"
   dynamodb_name = "${var.name}-dynamodb-lock-state"
@@ -8,7 +8,7 @@ module "backend" {
 }
 
 module "accounts" {
-  source = "../modules/accounts"
+  source = "../../modules/accounts"
 
   account_emails = var.account_emails
   account_names  = var.account_names
@@ -20,15 +20,13 @@ module "accounts" {
 }
 
 module "github_actions_roles_dev" {
-  source = "../modules/account_roles"
+  source = "../../modules/account_roles"
 
   providers = {
     aws = aws.dev
   }
 
-  account_id            = module.accounts.account_ids[0]
   management_account_id = module.accounts.organization_management_account_id
-  admin_role_name       = var.account_role_name
   role_name             = var.github_actions_role_name
   power_user_access     = var.github_actions_power_user_access[0]
 
@@ -36,15 +34,13 @@ module "github_actions_roles_dev" {
 }
 
 module "github_actions_roles_stage" {
-  source = "../modules/account_roles"
+  source = "../../modules/account_roles"
 
   providers = {
     aws = aws.stage
   }
 
-  account_id            = module.accounts.account_ids[1]
   management_account_id = module.accounts.organization_management_account_id
-  admin_role_name       = var.account_role_name
   role_name             = var.github_actions_role_name
   power_user_access     = var.github_actions_power_user_access[1]
 
@@ -52,15 +48,13 @@ module "github_actions_roles_stage" {
 }
 
 module "github_actions_roles_prod" {
-  source = "../modules/account_roles"
+  source = "../../modules/account_roles"
 
   providers = {
     aws = aws.prod
   }
 
-  account_id            = module.accounts.account_ids[2]
   management_account_id = module.accounts.organization_management_account_id
-  admin_role_name       = var.account_role_name
   role_name             = var.github_actions_role_name
   power_user_access     = var.github_actions_power_user_access[2]
 
@@ -68,15 +62,13 @@ module "github_actions_roles_prod" {
 }
 
 module "user_roles_dev" {
-  source = "../modules/account_roles"
+  source = "../../modules/account_roles"
 
   providers = {
     aws = aws.dev
   }
 
-  account_id            = module.accounts.account_ids[0]
   management_account_id = module.accounts.organization_management_account_id
-  admin_role_name       = var.account_role_name
   role_name             = var.user_role_name
   power_user_access     = var.user_power_user_access[0]
 
@@ -84,15 +76,13 @@ module "user_roles_dev" {
 }
 
 module "user_roles_stage" {
-  source = "../modules/account_roles"
+  source = "../../modules/account_roles"
 
   providers = {
     aws = aws.stage
   }
 
-  account_id            = module.accounts.account_ids[1]
   management_account_id = module.accounts.organization_management_account_id
-  admin_role_name       = var.account_role_name
   role_name             = var.user_role_name
   power_user_access     = var.user_power_user_access[1]
 
@@ -100,15 +90,13 @@ module "user_roles_stage" {
 }
 
 module "user_role_prod" {
-  source = "../modules/account_roles"
+  source = "../../modules/account_roles"
 
   providers = {
     aws = aws.prod
   }
 
-  account_id            = module.accounts.account_ids[2]
   management_account_id = module.accounts.organization_management_account_id
-  admin_role_name       = var.account_role_name
   role_name             = var.user_role_name
   power_user_access     = var.user_power_user_access[2]
 
